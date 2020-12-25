7d6ade65fdea92fe5b35128ee42509f1222438b7
Boralu <75725540+Boralu@users.noreply.github.com>
2020-12-25 16:38:35 +0530

    Add files via upload

---
+++ pclab/src/MyFrames/Product.java
@@ -0,0 +1,42 @@
+
+package MyFrames;
+
+
+public class Product {
+ 
+    
+
+    private String id;
+    private String pname;
+    private int available;
+    private int mrp;
+    
+    public Product(String id, String pname,int available, int mrp)
+    {
+        this.id=id;
+        this.pname=pname;
+        this.available=available;
+        this.mrp=mrp;
+        
+        
+    }
+            
+    public String getid()
+    {
+        return id;
+    }
+    public String getpname()
+    {
+        return pname;
+    }
+    public int getavailable()
+    {
+        return available;
+    }
+    public int getmrp()
+    {
+        return mrp;
+    }
+  }
+
+
